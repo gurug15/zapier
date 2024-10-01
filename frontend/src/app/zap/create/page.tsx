cb76784df5e1c8f30f6e2d08e26b24813f2b42be
GURUGASTGAR <gurugastgar@gmail.com>
2024-10-01 14:19:14 +0530

    commit

--- frontend/src/app/zap/create/page.tsx
+++ frontend/src/app/zap/create/page.tsx
@@ -1,7 +1,35 @@
+'use client'
+import AppBar from "@/components/AppBar";
+import DarkBtn from "@/components/btns/DarkBtn";
+import ZapCell from "@/components/ZapCell";
+import { useState } from "react";
 
 
-export default function page() {
+export default function Create() {
+  const [selectedTrigger, setSelectedTrigger] = useState("")
+  const [selectedActions, setSelectedActions]= useState<{
+      AvalableActionId: string,
+      AvalableActionName: string
+  }[]>([])
   return (
-    <div></div>
+    <div>
+      <AppBar/>
+      <div className="w-full min-h-screen bg-slate-200 flex flex-col justify-center items-center gap-y-2">
+      <ZapCell name={selectedTrigger ? selectedTrigger : "Trigger"} index={1}/>
+       <div className="w-full "> 
+        {selectedActions.map((action, idx)=>{
+            return <div key={idx} className="w-full flex justify-center">
+              <ZapCell name={action.AvalableActionName ? action.AvalableActionName : "Action"} index={2+idx} />
+            </div>   
+        })}
+       </div>
+       <DarkBtn onClick={()=>{
+        setSelectedActions(a => [...a, {
+          AvalableActionId: "",
+          AvalableActionName: ""
+        }])
+       }}><span className="text-2xl pb-1">+</span></DarkBtn>
+      </div>
+    </div>
   )
 }
--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 export default function AppBar() {
   const router = useRouter();
   return (
-    <div className="sticky top-0 left-0 right-0 flex justify-between border-b px-10 z-50 backdrop-blur-sm">
+    <div className="sticky top-0 left-0 right-0 flex justify-between border-b px-10 z-50 backdrop-blur-sm bg-white">
      <div className="pt-3">
      <Image width={0} height={0} className="w-[105px] h-[28px]" src="https://res.cloudinary.com/zapier-media/image/upload/q_auto/v1685628568/Zapier%20logos/zapier-logo-no-space_hihmgg.svg" alt="Logo"/>
      </div>
