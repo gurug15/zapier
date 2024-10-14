47e00043ad5f405afe4f8659a30cceb70f6bd77d
GURUGASTGAR <gurugastgar@gmail.com>
2024-10-14 20:49:19 +0530

    commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    NEXT_PUBLIC_HOOKS_URL: process.env.NEXT_PUBLIC_HOOKS_URL,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- frontend/src/app/config.ts
+++ frontend/src/app/config.ts
@@ -2,7 +2,6 @@
 
 export const  dynamic = 'force-dynamic';
 
-console.log(process.env.NEXT_PUBLIC_BACKEND_URL)
-export const BACKEND_URL = "http://backendzap:4001/api/v1";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const HOOKS_URL = process.env.NEXT_PUBLIC_HOOKS_URL ;
 
